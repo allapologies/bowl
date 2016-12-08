65ccd8faa7f289980ffbeafae3b79caa7f3c495d
aleksandr <alx.panteleev@gmail.com>
2016-12-08 18:46:41 +0300

    configure eslint, babel and webpack

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+ReactDOM.render(<div>HI!</div>, document.querySelector('.container'))
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+    entry  : {
+        app: ['./src/index.js']
+    },
+    output : {
+        path      : path.resolve(__dirname, 'build'),
+        publicPath: '/assets/',
+        filename  : 'bundle.js'
+    },
+    module : {
+        loaders: [
+            {
+                test   : /\.jsx?$/,
+                loader : 'babel',
+                exclude: /node_modules/
+            },
+            {
+                test  : /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+}
+
