aaadbfffda78ef73fa39e7ba1f06b4bc18239543
aleksandr <alx.panteleev@gmail.com>
2016-12-14 21:14:13 +0300

    add tests for frames reducer

--- tests/reducers.spec.js
+++ tests/reducers.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env jasmine */
 import players from '../reducers/reducer-players'
-import { GAME_ADD_PLAYER } from '../actions/constants'
+import frames from '../reducers/reducer-frames'
+import { GAME_ADD_PLAYER, GAME_INIT, GAME_START_FRAME, GAME_BALL_REACHED } from '../actions/constants'
 
 describe('Reducers', () => {
     describe('Players', () => {
@@ -42,5 +43,103 @@ describe('Reducers', () => {
             const actual = players(initialState, action)
             expect(actual).toEqual(expected)
         })
+    })
+    describe('Frames', () => {
+        it('should return the initial state', () => {
+            const expected = {
+                current: null,
+                roll: null,
+                data: {}
+            }
+
+            const actual = frames(undefined, {})
+            expect(actual).toEqual(expected)
+        })
+        it('should handle GAME_INIT', () => {
+            const expected = {
+                current: 1,
+                roll: 1,
+                data: {}
+            }
+
+            const action = {
+                type: GAME_INIT
+            }
+
+            const actual = frames(undefined, action)
+            expect(actual).toEqual(expected)
+        })
+        it('should handle GAME_START_FRAME', () => {
+            const expected = {
+                current: 6,
+                roll: 1,
+                data: {}
+            }
+
+            const state = {
+                current: 5,
+                roll: 2,
+                data: {}
+            }
+
+            const action = {
+                type: GAME_START_FRAME
+            }
+
+            const actual = frames(state, action)
+            expect(actual).toEqual(expected)
+        })
+        it('should handle GAME_THROW_BALL', () => {
+            const expected = {
+                current: 6,
+                roll: 1,
+                data: {}
+            }
+
+            const state = {
+                current: 5,
+                roll: 2,
+                data: {}
+            }
+
+            const action = {
+                type: GAME_START_FRAME
+            }
+
+            const actual = frames(state, action)
+            expect(actual).toEqual(expected)
+        })
+        it('should handle GAME_BALL_REACHED', () => {
+            const expected = {
+                current: 1,
+                roll: 1,
+                data: {
+                    1: {
+                        1: 5,
+                        2: 4
+                    }
+                }
+            }
+
+            const state = {
+                current: 1,
+                roll: 2,
+                data: {
+                    1: {
+                        1: 5
+                    }
+                }
+            }
+
+            const action = {
+                type: GAME_BALL_REACHED,
+                points: 4,
+                nextRoll: 1
+            }
+
+            const actual = frames(state, action)
+            expect(actual).toEqual(expected)
+        })
+
     })
 })
