4336da580db42b4710e3a21818729e5cab90b6de
aleksandr <alx.panteleev@gmail.com>
2017-01-27 21:20:49 +0300

    add redux-thunk to project

--- index.jsx
+++ index.jsx
@@ -1,16 +1,15 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import { createStore, compose, applyMiddleware } from 'redux'
 import reducers from './reducers'
 import { App } from './containers'
-import withCustomId from './middlewares/with-custom-id'
-import rollHandler from './middlewares/roll-handler'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(reducers, {}, composeEnhancers(
-  applyMiddleware(withCustomId, rollHandler)
+  applyMiddleware(thunk)
 ))
 
 ReactDOM.render(
