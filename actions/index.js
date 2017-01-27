e55bb69d110ba4ccf9f6b828a040635c5704aaa1
aleksandr <alx.panteleev@gmail.com>
2017-01-27 22:17:00 +0300

    rewrite middlewares chain to AC

--- actions/index.js
+++ actions/index.js
@@ -1,17 +1,16 @@
 import _ from 'lodash'
 import * as actions from './constants'
+import { getRandomInt, getNext, getMax } from '../utility/helpers'
 
-export function addPlayer (name) {
-    return function (dispatch) {
-        dispatch({
-            type: actions.GAME_ADD_PLAYER,
-            name,
-            id: _.uniqueId()
-        })
-    }
+export const addPlayer = (name) => (dispatch) => {
+    dispatch({
+        type: actions.GAME_ADD_PLAYER,
+        name,
+        id: _.uniqueId()
+    })
 }
 
-export function removePlayer (id) {
+export const removePlayer = (id) => {
     return {
         type: actions.GAME_REMOVE_PLAYER,
         id
@@ -28,7 +27,25 @@ export const startGame = () => ({ type: actions.START_NEW_GAME })
 
 export const replayGame = () => ({ type: actions.REPLAY_GAME })
 
-export const throwBall = () => ({
-    type: actions.GAME_THROW_BALL,
-    withRandomPoints: true
-})
+export const throwBall = () => (dispatch, getState) => {
+    dispatch({ type: actions.GAME_THROW_BALL })
+
+    const state = getState()
+    const { frames, players } = state
+    const { currentFrame, currentRoll, rolls } = frames
+    const { currentPlayer } = players
+
+    const max = getMax(rolls, currentPlayer, currentFrame)
+    const score = getRandomInt(0, max)
+
+    const nextState = getNext(state, score)
+
+    dispatch({
+        type: actions.GAME_THROW_BALL_SUCCESS,
+        score,
+        frameId: currentFrame,
+        rollId: currentRoll,
+        playerId: currentPlayer,
+        ...nextState
+    })
+}
