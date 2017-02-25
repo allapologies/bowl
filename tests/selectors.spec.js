6a9f2375a7cafb964fb4b1c6bd7d41b729501118
aleksandr <alx.panteleev@gmail.com>
2017-02-25 19:24:47 +0300

    add selector tests

--- tests/selectors.spec.js
+++ tests/selectors.spec.js
@@ -3,12 +3,9 @@ import { Map } from 'immutable'
 import { currentScoreSelector } from '../selectors/selectors'
 
 describe('selectors: ', () => {
-    describe('currentScore', () => {
-        it('exports', () => {
-            expect(currentScoreSelector).toBeDefined()
-        })
+    describe('currentScore returns score data for ', () => {
 
-        it('returns score data for first player', () => {
+        it('first player, first frame and first roll', () => {
 
             const expected = {
                 '1': [[2]]
@@ -30,5 +27,90 @@ describe('selectors: ', () => {
             expect(actual).toEqual(expected)
         })
 
+        it('first player, first frame and 2 rolls', () => {
+
+            const expected = {
+                '1': [[2, 3]]
+            }
+
+            const state = {
+                frames: Map({
+                    data: Map({
+                        '1': Map({
+                            '1': Map({
+                                '1': 2,
+                                '2': 3
+                            })
+                        })
+                    })
+                })
+            }
+
+            const actual = currentScoreSelector(state)
+            expect(actual).toEqual(expected)
+        })
+
+        it('first player, 2 frames and 1 roll', () => {
+
+            const expected = {
+                '1': [[2, 3], [1]]
+            }
+
+            const state = {
+                frames: Map({
+                    data: Map({
+                        '1': Map({
+                            '1': Map({
+                                '1': 2,
+                                '2': 3
+                            }),
+                            '2': Map({
+                                '1': 1
+                            })
+                        })
+                    })
+                })
+            }
+
+            const actual = currentScoreSelector(state)
+            expect(actual).toEqual(expected)
+        })
+
+        it('first 2 players, 2 frames', () => {
+
+            const expected = {
+                '1': [[2, 3], [1, 8]],
+                '2': [[5, 5], [0]]
+            }
+
+            const state = {
+                frames: Map({
+                    data: Map({
+                        '1': Map({
+                            '1': Map({
+                                '1': 2,
+                                '2': 3
+                            }),
+                            '2': Map({
+                                '1': 1,
+                                '2': 8
+                            })
+                        }),
+                        '2': Map({
+                            '1': Map({
+                                '1': 5,
+                                '2': 5
+                            }),
+                            '2': Map({
+                                '1': 0
+                            })
+                        })
+                    })
+                })
+            }
+
+            const actual = currentScoreSelector(state)
+            expect(actual).toEqual(expected)
+        })
     })
 })
