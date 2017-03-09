e3f8a8826af436da1f4262a72ed2f92f598888b4
Aleksandr <alx.panteleev@gmail.com>
2017-03-09 15:24:21 +0300

    fixes

--- reducers/reducer-frames.js
+++ reducers/reducer-frames.js
@@ -1,39 +1,40 @@
-import { Map, fromJS } from 'immutable'
-import _ from 'lodash'
+import { Map as iMap } from 'immutable'
+
 import * as actions from '../actions/constants'
 
-const INITIAL_STATE = Map({
+const INITIAL_STATE = iMap({
     currentFrame: null,
     currentRoll: null,
-    data: Map()
+    data: iMap()
 })
 
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
         case actions.GAME_START_ROLL:
             return state
-              .setIn(['currentFrame'], action.nextFrame)
-              .setIn(['currentRoll'], action.nextRoll)
+                .setIn(['currentFrame'], action.nextFrame)
+                .setIn(['currentRoll'], action.nextRoll)
         case actions.GAME_INIT:
             return state
-              .setIn(['currentFrame'], 1)
-              .setIn(['currentRoll'], 1)
+                .setIn(['currentFrame'], 1)
+                .setIn(['currentRoll'], 1)
         case actions.GAME_START_FRAME:
             return state
-              .setIn(['currentFrame'], action.frameId)
-              .setIn(['currentRoll'], 1)
+                .setIn(['currentFrame'], action.frameId)
+                .setIn(['currentRoll'], 1)
         case actions.GAME_THROW_BALL_SUCCESS:
             return state
-              .updateIn(['data', action.playerId], (arr) => arr.push(Map({
-                    frameId: action.frameId,
-                    rollId: action.rollId,
-                    score: action.score
-                }))
-              )
+                .updateIn(['data', action.playerId], (arr) =>
+                    arr.push(iMap({
+                        frameId: action.frameId,
+                        rollId: action.rollId,
+                        score: action.score
+                    }))
+                )
         case actions.START_NEW_GAME:
             return state
-              .setIn(['currentFrame'], 1)
-              .setIn(['currentRoll'], 1)
+                .setIn(['currentFrame'], 1)
+                .setIn(['currentRoll'], 1)
         default:
             return state
     }
--- reducers/tests/reducer-frames.spec.js
+++ reducers/tests/reducer-frames.spec.js
@@ -1,4 +1,3 @@
-/* eslint-env jasmine */
 import { Map, List, fromJS } from 'immutable'
 import * as matchers from 'jasmine-immutable-matchers'
 import frames from '../reducer-frames'
@@ -53,34 +52,13 @@ describe('Reducer - frames', () => {
         expect(actual.toJS()).toEqual(expected)
     })
 
-    xit('creates properties in data map when starting the game', () => {
-        const expected = {
+    it('handle THROW BALL SUCCESS for first roll', () => {
+
+        const state = Map({
             currentFrame: null,
             currentRoll: null,
-            data: {
-                1: {},
-                2: {},
-                10: {},
-                55: {}
-            }
-        }
-
-        const action = {
-            type: START_NEW_GAME,
-            players: [
-                { name: 'Aleksandr', id: 1 },
-                { name: 'Irina', id: 2 },
-                { name: 'Michael', id: 10 },
-                { name: 'Kurt', id: 55 },
-            ]
-        }
-
-        const actual = frames(undefined, action)
-
-        expect(actual).toEqualImmutable(expected)
-    })
-
-    xit('handle THROW BALL SUCCESS for first roll', () => {
+            data: Map({ '1': List([])})
+        })
 
         const action = {
             type: GAME_THROW_BALL_SUCCESS,
@@ -104,7 +82,7 @@ describe('Reducer - frames', () => {
             }
         })
 
-        const actual = frames(undefined, action)
+        const actual = frames(state, action)
 
         expect(actual).toEqualImmutable(expected)
     })
@@ -149,64 +127,6 @@ describe('Reducer - frames', () => {
                     })
                 ])
             })
-        })
-
-        const actual = frames(state, action)
-        expect(actual).toEqualImmutable(expected)
-    })
-
-    xit('handle THROW BALL SUCCESS for different players', () => {
-        const state = fromJS({
-            currentFrame: null,
-            currentRoll: null,
-            data: {
-                '2': [
-                    {
-                        frameId: 1,
-                        rollId: 1,
-                        score: 4
-                    },
-                    {
-                        frameId: 1,
-                        rollId: 2,
-                        score: 3
-                    }
-                ]
-            }
-        })
-
-        const action = {
-            type: GAME_THROW_BALL_SUCCESS,
-            playerId: '2',
-            frameId: 2,
-            rollId: 1,
-            score: 8
-        }
-
-        const expected = fromJS({
-            currentFrame: null,
-            currentRoll: null,
-            data: {
-                '5': [
-                    {
-                        frameId: 1,
-                        rollId: 1,
-                        score: 4
-                    },
-                    {
-                        frameId: 3,
-                        rollId: 2,
-                        score: 2
-                    }
-                ],
-                '2': [
-                    {
-                        frameId: 2,
-                        rollId: 1,
-                        score: 8
-                    }
-                ]
-            }
         })
 
         const actual = frames(state, action)
