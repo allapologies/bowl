574998a534f7deefc52d61e118e3b1c20f119d48
aleksandr <alx.panteleev@gmail.com>
2017-03-22 22:34:04 +0300

    set eslint as dependency

--- eslintrc.js
+++ eslintrc.js
@@ -1,55 +1,10 @@
 module.exports = {
-  globals: {
-    DEBUG: true
-  },
-  env: {
-    browser: true,
-    es6: true,
-    jasmine: true
-  },
-  extends: [
-    './.eslintRules/possible-errors',
-    './.eslintRules/best-practices',
-    './.eslintRules/strict-mode',
-    './.eslintRules/variables',
-    './.eslintRules/node-js-and-common-js',
-    './.eslintRules/stylistic',
-    './.eslintRules/es6',
-    './.eslintRules/react',
-    './.eslintRules/import',
-  ].map(require.resolve),
-  ecmaFeatures: {
-    arrowFunctions: true,
-    blockBindings: true,
-    classes: true,
-    defaultParams: true,
-    destructuring: true,
-    jsx: true,
-    modules: true,
-    objectLiteralShorthandMethods: true,
-    objectLiteralShorthandProperties: true,
-    spread: true,
-    templateStrings: true,
-    experimentalObjectRestSpread: true
-  },
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-    ecmaFeatures: {
-      arrowFunctions: true,
-      blockBindings: true,
-      classes: true,
-      defaultParams: true,
-      destructuring: true,
-      jsx: true,
-      modules: true,
-      objectLiteralShorthandMethods: true,
-      objectLiteralShorthandProperties: true,
-      spread: true,
-      templateStrings: true,
-      experimentalObjectRestSpread: true
-    },
-  },
-  rules: {
-  }
+    extends: 'sbtsbol',
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: './node_modules/webpack-config-sbtsbol/eslint.js'
+            }
+        }
+    }
 }
